9952abb82cb2f5992a21f5216bbd4e54dc122c7a
FRANCISCO WENDEL ALMEIDA CAVALCANTE <wendel@tre-ce.jus.br>
2024-01-24 13:15:59 -0300

    Initial Commit

---
+++ src/main/java/br/jus/trece/regulusApi/Controller.java
@@ -0,0 +1,14 @@
+package br.jus.trece.regulusApi;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Controller {
+
+	@GetMapping("/")
+	public String index() {
+		return "Greetings from Spring Boot!";
+	}
+
+}
